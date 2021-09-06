bb2a8585e3baba2cf02d850421ff14454a32023e
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2021-09-06 13:49:23 -0300

    Projeto inicial: Security, context, entity

---
+++ BACKEND/src/main/java/com/JCM/Pessoa/entity/Contato.java
@@ -0,0 +1,83 @@
+package com.JCM.Pessoa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_contato")
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String telefone;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@ManyToOne
+	private Pessoa pessoa;
+
+	public Contato() {
+
+	}
+
+	public Contato(Long id, String nome, String telefone, String email, Pessoa pessoa) {
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+		this.pessoa = pessoa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+}
---
+++ BACKEND/src/main/java/com/JCM/Pessoa/entity/Pessoa.java
@@ -0,0 +1,88 @@
+package com.JCM.Pessoa.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_Pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private LocalDate datanasc;
+	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pessoa")
+	private List<Contato> contatos;
+
+	public Pessoa() {
+	
+	}
+
+	public Pessoa(Long id, String nome, String cpf, LocalDate datanasc, List<Contato> contatos) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.datanasc = datanasc;
+		this.contatos = contatos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDatanasc() {
+		return datanasc;
+	}
+
+	public void setDatanasc(LocalDate datanasc) {
+		this.datanasc = datanasc;
+	}
+
+	public List<Contato> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Contato> contatos) {
+		this.contatos = contatos;
+	}
+
+}
